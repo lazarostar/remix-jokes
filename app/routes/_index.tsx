2d31b671960c684e890c6ff10a6e017f8f40b7b8
lazarostar <nascimento.dev26@gmail.com>
2023-08-12 12:33:08 -0300

    feat: styles

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,14 @@
+import { LinksFunction } from "@remix-run/node";
+
+import stylesUrl from "../styles/index.css";
+
+export const links: LinksFunction = () => [
+  {
+    rel: "stylesheet",
+    href: stylesUrl,
+  },
+];
+
 export default function IndexRoute() {
   return <div>Hello Index Route</div>;
 }
